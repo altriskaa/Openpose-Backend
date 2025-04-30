d1c87d550309b9b827b788dc52819339203dff72
Danar <114123567+Danar1111@users.noreply.github.com>
2025-04-30 23:39:12 +0700

    update

--- app/services/pose_estimation.py
+++ app/services/pose_estimation.py
@@ -3,17 +3,17 @@ import numpy as np
 from openpose import pyopenpose as op
 from app.utils.image_converter import bytes_to_cv2
 
-# Konfigurasi OpenPose
-params = {
-    "model_folder": "/root/openpose/models/",
-    "net_resolution": "-1x368",
-    "disable_multi_thread": True,
-}
-opWrapper = op.WrapperPython()
-opWrapper.configure(params)
-opWrapper.start()
-
 def process_pose_from_bytes(image_bytes):
+    # Konfigurasi OpenPose
+    params = {
+        "model_folder": "/root/openpose/models/",
+        "net_resolution": "-1x368",
+        "disable_multi_thread": True,
+    }
+    opWrapper = op.WrapperPython()
+    opWrapper.configure(params)
+    opWrapper.start()
+    
     image = bytes_to_cv2(image_bytes)
 
     datum = op.Datum()
