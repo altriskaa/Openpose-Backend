579b8c23bdc33df76e0cf75897b8cabd4da264fa
Danar <114123567+Danar1111@users.noreply.github.com>
2025-04-30 23:50:16 +0700

    update

--- app/services/pose_estimation.py
+++ app/services/pose_estimation.py
@@ -13,12 +13,19 @@ def process_pose_from_bytes(image_bytes):
     opWrapper = op.WrapperPython()
     opWrapper.configure(params)
     opWrapper.start()
-    
+
     image = bytes_to_cv2(image_bytes)
 
     datum = op.Datum()
     datum.cvInputData = image
-    opWrapper.emplaceAndPop([datum])
+    datum_ptr = op.Datum()
+    datum_ptr.cvInputData = image
+
+    datums = op.VectorDatum()
+    datums.append(datum_ptr)
+
+    opWrapper.emplaceAndPop(datums)
+
 
     # Misalnya outputnya adalah keypoints
     keypoints = datum.poseKeypoints.tolist() if datum.poseKeypoints is not None else []
