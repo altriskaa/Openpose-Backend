95c29e9efad6fa8fcbfbddcd9f4ac9e4e3e134f7
Danar <114123567+Danar1111@users.noreply.github.com>
2025-05-05 15:25:28 +0700

    routing get image

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, send_from_directory
 from app.services.pose_estimation import process_pose_from_bytes
+import os
 
 pose_bp = Blueprint('pose', __name__)
 
@@ -16,3 +17,8 @@ def predict_image():
         return jsonify({"result": result})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+@pose_bp.route('/output_images/<path:filename>')
+def serve_output_image(filename):
+    directory = os.path.join(os.getcwd(), 'output_images')
+    return send_from_directory(directory, filename, mimetype='image/png')
